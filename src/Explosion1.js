5f892682fddde0e1c4bd6b49d3b387aaf6b4e8a2
bigfanjs <bigfanjs@gmail.com>
2018-04-12 16:13:58 +0100

    feat: allows users to listen for animation start and finish events

--- src/Explosion1.js
+++ src/Explosion1.js
@@ -14,14 +14,13 @@ class Explosion extends Component {
 
     componentDidMount() {
         const ease = Power4.easeOut;
+        const angle = Math.PI / 8;
         const radius = this.size * this.radius / 100;
         const durations = this.durations;
         const timeleft = durations[1] - durations[0];
-        const { delay = 0, repeat = 0, repeatDelay = 0 } = this.props;
+        const { delay = 0, repeat = 0, repeatDelay = 0, onComplete, onStart } = this.props;
 
         for (let i = 0; i < this.count; i++) {
-            const angle = Math.PI / 8;
-
             const x = this.center + radius * Math.cos(i * angle);
             const y = this.center + radius * Math.sin(i * angle);
 
@@ -30,8 +29,22 @@ class Explosion extends Component {
             const start = { x2: x, y2: y };
             const end = { x1: x, y1: y };
 
-            TweenMax.to(target, durations[0], { attr: start, ease, repeat, delay, repeatDelay: repeatDelay + timeleft });
-            TweenMax.to(target, durations[1], { attr: end, ease, repeat, delay, repeatDelay });
+            TweenMax.to(target, durations[0],
+                {
+                    attr: start,
+                    ease, repeat, delay,
+                    repeatDelay: repeatDelay + timeleft
+                }
+            );
+            TweenMax.to(target, durations[1],
+                {
+                    ease, repeat, delay,
+                    attr: end,
+                    repeatDelay,
+                    onComplete: onComplete && i >= this.count - 1 && onComplete,
+                    onStart: onStart && i >= this.count - 1 && onStart
+                }
+            );
         }
     }
 
