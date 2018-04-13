8b95176096585af227a63420108e1d3d38b17838
bigfanjs <bigfanjs@gmail.com>
2018-04-13 11:07:27 +0100

    feat: changes the Explosion2's public interface

--- src/Explosion2.js
+++ src/Explosion2.js
@@ -1,31 +1,31 @@
 import React, { Component, Fragment } from "react";
 import { TweenLite, Power4 } from "gsap";
 
-import "./Explosion2.css";
-
 class Explosion extends Component {
-    targets = [];
+    size = this.props.size;
+    center = this.size / 2;
 
     componentDidMount() {
-        const { duration, delay = 0, radius } = this.props;
+        const { delay = 0 } = this.props;
         const ease = Power4.easeOut;
 
-        TweenLite.to(this.circle, duration, { attr: { r: radius }, delay, ease });
-        TweenLite.to(this.circle, duration / 2, { attr: { "stroke-width": 0 }, delay: duration / 2, ease });
+        TweenLite.to(this.circle, 1, { attr: { r: 190 }, ease });
+        TweenLite.to(this.circle, 0.8, { attr: { "stroke-width": 0 }, delay: 0.5, ease });
     }
 
     render() {
-        const { radius, style, width, className="" } = this.props;
-        const size = radius * 2 + width;
-        const origin = size / 2;
+        const size = this.size;
+        const center = this.center;
+
+        const { style } = this.props;
 
         return (
-            <svg className={className} style={style} width={size} height={size}>
+            <svg style={style} width={size} height={size}>
                 <circle
-                    cx={origin}
-                    cy={origin}
+                    cx={center}
+                    cy={center}
                     r={0}
-                    strokeWidth={width}
+                    strokeWidth="2"
                     stroke="white"
                     fill="none"
                     ref={(el) => this.circle = el}
