bb1b73f5afb5b7c3afeec8eb7d50dee0801b61a9
bigfanjs <bigfanjs@gmail.com>
2018-04-13 11:15:20 +0100

    feat: makes explosion2 component responsive

--- src/Explosion2.js
+++ src/Explosion2.js
@@ -5,27 +5,31 @@ class Explosion extends Component {
     size = this.props.size;
     center = this.size / 2;
 
+    radius = 47.5;
+    strokeWidth = 1;
+
     componentDidMount() {
         const { delay = 0 } = this.props;
         const ease = Power4.easeOut;
+        const radius = this.size * this.radius / 100;
 
-        TweenLite.to(this.circle, 1, { attr: { r: 190 }, ease });
+        TweenLite.to(this.circle, 1, { attr: { r: radius }, ease });
         TweenLite.to(this.circle, 0.8, { attr: { "stroke-width": 0 }, delay: 0.5, ease });
     }
 
     render() {
         const size = this.size;
         const center = this.center;
-
         const { style } = this.props;
+        const strokeWidth = Math.ceil(size * this.strokeWidth / 100);
 
         return (
-            <svg style={style} width={size} height={size}>
+            <svg width={size} height={size}>
                 <circle
                     cx={center}
                     cy={center}
                     r={0}
-                    strokeWidth="2"
+                    strokeWidth={strokeWidth}
                     stroke="white"
                     fill="none"
                     ref={(el) => this.circle = el}
