d270dbb5be01f1fc889895ec4e41386b02feb669
bigfanjs <bigfanjs@gmail.com>
2018-04-15 11:49:42 +0100

    feat: makes explosion3 reponsive

--- src/Explosion4.js
+++ src/Explosion4.js
@@ -1,29 +1,31 @@
 import React, { Component, Fragment } from "react";
 import { TweenLite, Power4 } from "gsap";
 
-import "./Explosion2.css";
-
 class Explosion extends Component {
+    size = this.props.size;
+    center = this.size / 2;
     targets = [];
     counts = [7, 32];
     widths = [7, 2];
+    radius = 47.5;
     circles = [
-        { el: null, pos: [0, 0], radius: 100 },
-        { el: null, pos: [22, -13], radius: 160 },
-        { el: null, pos: [-22, -1], radius: 130 }
+        { el: null, pos: [50, 50], radius: 25 },
+        { el: null, pos: [68, 46], radius: 40 },
+        { el: null, pos: [30, 60], radius: 32.5 }
     ]
 
     componentDidMount() {
-        const { delay = 0, radius = 100 } = this.props;
+        const { delay = 0 } = this.props;
         const duration = 0.5;
         const ease = Power4.easeOut;
+        const radius = this.size * this.radius / 100;
 
         for (let i = 0; i < 2; i++) {
             const angle = Math.PI / (this.counts[i] / 2);
 
             for (let j = 0; j < this.counts[i]; j++) {
-                const x = radius + radius * Math.cos(j * angle);
-                const y = radius + radius * Math.sin(j * angle);
+                const x = this.center + radius * Math.cos(j * angle);
+                const y = this.center + radius * Math.sin(j * angle);
 
                 const target = this.targets[i][j];
 
@@ -38,7 +40,9 @@ class Explosion extends Component {
         }
 
         for (let i = 0; i < this.circles.length; i++) {
-            const { el, radius } = this.circles[i];
+            const circle = this.circles[i];
+            const el = circle.el;
+            const radius = this.size * circle.radius / 100;
 
             TweenLite.to(el, duration * 2, { attr: { r: radius / 2 }, delay: (i + 1) * 0.2, ease });
             TweenLite.to(el, duration * 2.5, { attr: { "stroke-width": 0 }, delay: (i + 1) * 0.2, ease });
@@ -46,8 +50,8 @@ class Explosion extends Component {
     }
 
     render() {
-        const radius = this.props.radius;
-        const size = radius * 2;
+        const size = this.size;
+        const center = this.center;
 
         return (
             <svg width={size} height={size}>
@@ -59,10 +63,10 @@ class Explosion extends Component {
                             <Fragment key={i}>
                                 {[...Array(this.counts[i])].map((_, j) =>
                                     <line
-                                        x1={radius}
-                                        y1={radius}
-                                        x2={radius}
-                                        y2={radius}
+                                        x1={center}
+                                        y1={center}
+                                        x2={center}
+                                        y2={center}
                                         ref={(el) => this.targets[i][j] = el}
                                         key={j}
                                         strokeWidth={this.widths[i]}
@@ -75,19 +79,18 @@ class Explosion extends Component {
                 <Fragment>
                     {[...Array(3)].map((_, i) => {
                         const position = this.circles[i].pos;
-                        const x = position[0];
-                        const y = position[1];
+                        const x = size * position[0] / 100;
+                        const y = size * position[1] / 100;
 
                         return (
                             <circle
-                                style={{ transform: `translate(${x}%, ${y}%)` }}
                                 key={i}
-                                cx={radius}
-                                cy={radius}
+                                cx={x}
+                                cy={y}
                                 r={0}
                                 strokeWidth="4"
-                                stroke="white"
                                 fill="none"
+                                stroke="white"
                                 ref={(el) => this.circles[i].el = el}
                             />
                         );
