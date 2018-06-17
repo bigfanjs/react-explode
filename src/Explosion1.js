847650302dfc56c136fe552a7421a33f81d533af
bigfanjs <bigfanjs@gmail.com>
2018-06-17 13:19:02 +0100

    feat: updates Explosion1 when receives new props

--- src/Explosion1.js
+++ src/Explosion1.js
@@ -1,10 +1,12 @@
 import React, { Component, Fragment } from "react";
-import { TweenMax, Power4 } from "gsap";
+import { TimelineMax, Power4 } from "gsap";
 
 class Explosion extends Component {
-    size = this.props.size;
-    center = this.size / 2;
+    state = { size: 400, delay: 0, repeatDelay: 0, repeat: 0 };
 
+    size = this.props.size;
+
+    timeline = null;
     lines = [];
     count = 16;
     radius = 47.5;
@@ -12,65 +14,90 @@ class Explosion extends Component {
 
     durations = [0.6, 1];
 
+    static getDerivedStateFromProps({size, delay, repeatDelay, repeat}, prevState) {
+        if (size !== prevState.size               ||
+            delay !== prevState.delay             ||
+            repeatDelay !== prevState.repeatDelay ||
+            repeat !== prevState.repeat
+        ) return { size, delay, repeatDelay, repeat };
+
+        return null;
+    }
+
     componentDidMount() {
+        this.explode();
+    }
+
+    componentDidUpdate() {
+        this.timeline.kill();
+        this.explode();
+    }
+
+    explode() {
+        const { size, repeat, delay, repeatDelay } = this.state;
         const ease = Power4.easeOut;
         const angle = Math.PI / 8;
-        const radius = this.size * this.radius / 100;
+        const radius = size * this.radius / 100;
         const durations = this.durations;
-        const timeleft = durations[1] - durations[0];
-        const { delay = 0, repeat = 0, repeatDelay = 0, onComplete, onStart, onRepeat } = this.props;
+        const { onComplete, onStart, onRepeat } = this.props;
+        const center = size / 2;
+        const timelines = [];
+
+        this.timeline = new TimelineMax({
+            repeat, delay, repeatDelay, onComplete, onStart, onRepeat
+        });
 
         for (let i = 0; i < this.count; i++) {
-            const x = this.center + radius * Math.cos(i * angle);
-            const y = this.center + radius * Math.sin(i * angle);
-
+            const x = center + radius * Math.cos(i * angle);
+            const y = center + radius * Math.sin(i * angle);
             const target = this.lines[i];
-
             const start = { x2: x, y2: y };
             const end = { x1: x, y1: y };
 
-            TweenMax.to(target, durations[0],
-                {
-                    attr: start,
-                    ease, repeat, delay,
-                    repeatDelay: repeatDelay + timeleft
-                }
-            );
-            TweenMax.to(target, durations[1],
-                {
-                    ease, repeat, delay,
-                    attr: end,
-                    repeatDelay,
-                    onComplete: onComplete && i >= this.count - 1 && onComplete,
-                    onStart: onStart && i >= this.count - 1 && onStart,
-                    onRepeat: onRepeat && i >= this.count - 1 && onRepeat
-                }
-            );
+            const timeline = new TimelineMax();
+
+            timeline
+                .fromTo(
+                    target,
+                    durations[0],
+                    { attr: { x2: center, y2: center } },
+                    { attr: start, ease }
+                )
+                .fromTo(
+                    target,
+                    durations[1],
+                    { attr: {x1: center, y1: center} },
+                    { attr: end, ease },
+                    `-=${durations[0]}`
+                );
+
+            timelines.push(timeline);
         }
+
+        this.timeline.add(timelines);
     }
 
     render() {
-        const { size, style, color = "white" } = this.props;
-        const center = this.center;
-        const strokeWidth = Math.ceil(this.size * this.strokeWidth / 100);
+        const size = this.state.size;
+        const {color="white", style} = this.props;
+        const center = size / 2;
+        const strokeWidth = Math.ceil(size * this.strokeWidth / 100);
 
         return (
             <svg width={size} height={size} style={style}>
                 <Fragment>
-                    {[...Array(this.count)].map((_, i) => {
-                        return (
-                            <line
-                                x1={center}
-                                y1={center}
-                                x2={center}
-                                y2={center}
-                                ref={(el) => this.lines[i] = el}
-                                key={i}
-                                strokeWidth={strokeWidth}
-                                stroke={color}
-                            />
-                        )
-                    })}
+                    {[...Array(this.count)].map((_, i) =>
+                        <line
+                            x1={center}
+                            y1={center}
+                            x2={center}
+                            y2={center}
+                            ref={(el) => this.lines[i] = el}
+                            key={i}
+                            strokeWidth={strokeWidth}
+                            stroke={color}
+                        />
+                    )}
                 </Fragment>
             </svg>
         );
