7a3d3cb7106dcc50172c34a6aa4d686d270867c2
bigfanjs <bigfanjs@gmail.com>
2018-06-18 17:06:22 +0100

    fix: updates Explosion5 when receices new props

--- src/Explosion5.js
+++ src/Explosion5.js
@@ -5,62 +5,129 @@ class Explosion extends Component {
     constructor(props) {
         super(props);
 
-        const size = this.props.size;
+        this.state = { size: 400, delay: 0, repeatDelay: 0, repeat: 0 };
 
+        this.timeline = null;
         this.squares = [];
-        const sizes = this.sizes = [
+        this.strokeWidth = 2.5;
+    }
+
+    static getDerivedStateFromProps({ size, delay, repeatDelay, repeat }, prevState) {
+        if (size !== prevState.size               ||
+            delay !== prevState.delay             ||
+            repeatDelay !== prevState.repeatDelay ||
+            repeat !== prevState.repeat
+        ) return { size, delay, repeatDelay, repeat };
+
+        return null;
+    }
+
+    shouldComponentUpdate({ size, delay, repeatDelay, repeat }) {
+        const state = this.state;
+
+        return (
+            size !== state.size               ||
+            delay !== state.delay             ||
+            repeatDelay !== state.repeatDelay ||
+            repeat !== state.repeat
+        );
+    }
+
+    componentDidMount() {
+        this.explode();
+    }
+
+    componentDidUpdate() {
+        this.timeline.kill();
+        this.explode();
+    }
+
+    explode = () => {
+        const ease = Power4.easeOut;
+        const { size, delay, repeat, repeatDelay } = this.state;
+        const { onStart, onComplete, onRepeat } = this.props;
+        const center = size / 2;
+        const sizes = [
             size * 0.58,
             size * 0.3,
             size * 0.3,
             size * 0.1,
             size * 0.1
         ];
-        this.halfs = [
-            sizes[0] / 2,
-            sizes[1] / 2,
-            sizes[3] / 2
-        ];
-    }
-
-    componentDidMount() {
-        const ease = Power4.easeOut;
-        const { size, delay = 0, repeat = 0, repeatDelay = 0, onStart, onComplete, onRepeat } = this.props;
-        const center = size / 2;
-        const halfs = this.halfs;
+        const halfs = [sizes[0] / 2, sizes[1] / 2, sizes[3] / 2];
         const front = center - halfs[0];
         const back = center + halfs[0];
-        const positions = [
-            { x: front, y: front },
-            { x: center - halfs[1], y: back - halfs[1] },
-            { x: center - halfs[1], y: front - halfs[1] },
-            { x: front - halfs[2], y: front - halfs[2] },
-            { x: back - halfs[2], y: back - halfs[2] }
-        ];
+        const positions = {
+            start: [
+                { x: center, y: center },
+                { x: center, y: back },
+                { x: center, y: front },
+                { x: front, y: front },
+                { x: back, y: back }
+            ],
+            end: [
+                { x: front, y: front },
+                { x: center - halfs[1], y: back - halfs[1] },
+                { x: center - halfs[1], y: front - halfs[1] },
+                { x: front - halfs[2], y: front - halfs[2] },
+                { x: back - halfs[2], y: back - halfs[2] }
+            ]
+        };
+        const timelines = [];
+
         const delays = [0, 0.3, 0.3, 0.5, 0.5];
+        const strokeWidth = Math.ceil(size * this.strokeWidth / 100);
 
         for (let i = 0; i < this.squares.length; i++) {
             const square = this.squares[i];
-            const size = this.sizes[i];
-            const { x, y } = positions[i];
-            const delay2 = delays[i];
-            const tl = new TimelineMax({
-                delay: delay + delay2, repeat, repeatDelay,
+            const size = sizes[i];
+            const start = positions.start[i];
+            const end = positions.end[i];
+            const delay = delays[i];
+            const timeline = new TimelineMax({
+                delay,
                 onStart: i === 0 && onStart,
                 onComplete: i === 0 && onComplete,
                 onRepeat: i === 0 && onRepeat
             });
 
-            tl.to(square, 1, { attr: { x, y, width: size, height: size }, ease });
-            tl.to(square, 1, { attr: { "stroke-width": 0 }, ease }, "-=0.9");
+            timeline
+                .fromTo(
+                    square,
+                    1,
+                    { attr: { x: start.x, y: start.y, width: 0, height: 0 }, ease },
+                    { attr: { x: end.x, y: end.y, width: size, height: size }, ease }
+                )
+                .fromTo(
+                    square,
+                    1,
+                    { attr: { "stroke-width": strokeWidth }, ease },
+                    { attr: { "stroke-width": 0 }, ease },
+                    "-=0.9"
+                );
+
+                timelines.push(timeline);
         }
-    }
+
+        this.timeline = new TimelineMax({ delay, repeat, repeatDelay });
+        this.timeline.add(timelines);
+    };
 
     render() {
-        const {size, style} = this.props;
+        const size = this.state.size;
+        const style = this.props.style;
         const center = size / 2;
-        const front = center - this.halfs[0];
-        const back = center + this.halfs[0];
-
+        const sizes = [
+            size * 0.58,
+            size * 0.3,
+            size * 0.3,
+            size * 0.1,
+            size * 0.1
+        ];
+        const halfs = [sizes[0] / 2, sizes[1] / 2, sizes[3] / 2];
+        const front = center - halfs[0];
+        const back = center + halfs[0];
+        const strokeWidth = Math.ceil(size * this.strokeWidth / 100);
         const positions = [
             { x: center, y: center },
             { x: center, y: back },
@@ -71,7 +138,7 @@ class Explosion extends Component {
 
         return (
             <svg width={size} height={size} style={style}>
-                {[...Array(5)].map((square, i) => {
+                {[...Array(5)].map((_, i) => {
                     const { x, y } = positions[i];
 
                     return (
@@ -82,7 +149,7 @@ class Explosion extends Component {
                             width={0}
                             height={0}
                             stroke="white"
-                            strokeWidth="10"
+                            strokeWidth={strokeWidth}
                             fill="none"
                             ref={(el) => this.squares[i] = el}
                             style={{ transform: "rotate(-50deg)", transformOrigin: `${center}px ${center}px` }}
