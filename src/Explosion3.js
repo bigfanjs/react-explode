899126e207bb35cbf120263b850a431fd1dc3730
bigfanjs <bigfanjs@gmail.com>
2018-06-18 15:11:49 +0100

    fix: updates Explosion3 when receices new props

--- src/Explosion3.js
+++ src/Explosion3.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from "react";
 import { TimelineMax, Power4 } from "gsap";
 
 class Explosion extends Component {
+    state = { size: 400, delay: 0, repeatDelay: 0, repeat: 0 };
     targets = [[]];
 
     size = this.props.size;
@@ -14,55 +15,113 @@ class Explosion extends Component {
 
     count = 16;
 
+    static getDerivedStateFromProps({ size, delay, repeatDelay, repeat }, prevState) {
+        if (size !== prevState.size               ||
+            delay !== prevState.delay             ||
+            repeatDelay !== prevState.repeatDelay ||
+            repeat !== prevState.repeat
+        ) return { size, delay, repeatDelay, repeat };
+
+        return null;
+    }
+
+    shouldComponentUpdate({ size, delay, repeatDelay, repeat }) {
+        const state = this.state;
+
+        return (
+            size !== state.size               ||
+            delay !== state.delay             ||
+            repeatDelay !== state.repeatDelay ||
+            repeat !== state.repeat
+        );
+    }
+
     componentDidMount() {
-        const { delay = 0, repeat = 0, repeatDelay = 0, onStart, onComplete, onRepeat } = this.props;
+        this.explode();
+    }
+
+    componentDidUpdate() {
+        this.timeline.kill();
+        this.explode();
+    }
+
+    explode = () => {
+        const { size, delay, repeat, repeatDelay } = this.state;
+        const { onStart, onComplete, onRepeat } = this.props;
         const angle = Math.PI / (this.count / 2);
         const ease = Power4.easeOut;
-        const radiuses = this.radiuses.map((radius) => this.size * radius / 100);
+        const radiuses = this.radiuses.map((radius) => size * radius / 100);
+        const center = size / 2;
         const timeline = new TimelineMax({
-            repeat, repeatDelay,
-            delay: delay + 0.35,
+            delay: 0.35,
             onComplete: onComplete && onComplete.bind(null, 2),
             onStart: onStart && onStart.bind(null, 2),
             onRepeat: onRepeat && onRepeat.bind(null, 2)
         });
+        const timelines = [];
+        const circleStrokeWidth = Math.ceil(size * this.circleStrokeWidth / 100);
 
         for (let i = 0; i < 2; i++) {
             for (let j = 0; j < this.count; j++) {
                 const isLast = j >= this.count - 1;
 
                 const timeline = new TimelineMax({
-                    repeat, repeatDelay,
-                    delay: delay + i * 0.5,
+                    delay: i * 0.5,
                     onComplete: onComplete && isLast && onComplete.bind(null, i),
                     onStart: onStart && isLast && onStart.bind(null, i),
                     onRepeat: onRepeat && isLast && onRepeat.bind(null, i)
                 });
 
-                const x = this.center + radiuses[0] * Math.cos(j * angle);
-                const y = this.center + radiuses[0] * Math.sin(j * angle);
+                const x = center + radiuses[0] * Math.cos(j * angle);
+                const y = center + radiuses[0] * Math.sin(j * angle);
 
                 const target = this.targets[i][j];
 
                 const start = { x2: x, y2: y };
                 const end = { x1: x, y1: y };
 
-                timeline.to(target, 1, { attr: start, ease })
-                timeline.to(target, 1, { attr: end, ease }, "-=0.9");
+                timeline
+                    .fromTo(
+                        target,
+                        1,
+                        { attr: { x2: center, y2: center } },
+                        { attr: start, ease }
+                    )
+                    .fromTo(
+                        target,
+                        1,
+                        { attr: { x1: center, y1: center } },
+                        { attr: end, ease },
+                        "-=0.9"
+                    );
+
+                timelines.push(timeline);
             }
         }
 
-        timeline.to(this.circle, 1, { attr: { r: radiuses[1] }, ease });
-        timeline.to(this.circle, 1, { attr: { "stroke-width": 0 }, ease }, "-=0.9");
+        timeline
+            .fromTo(this.circle, 1, { attr: { r: 0 } }, { attr: { r: radiuses[1] }, ease })
+            .fromTo(
+                this.circle,
+                1,
+                { attr: { "stroke-width": circleStrokeWidth } },
+                { attr: { "stroke-width": 0 }, ease },
+                "-=0.9"
+            );
+
+        timelines.push(timeline);
+
+        this.timeline = new TimelineMax({ delay, repeat, repeatDelay });
+        this.timeline.add(timelines);
     }
 
     render() {
-        const size = this.size;
-        const center = this.center;
+        const size = this.state.size;
+        const center = size / 2;
         const { style, color = "white" } = this.props;
 
-        const circleStrokeWidth = Math.ceil(this.size * this.circleStrokeWidth / 100);
-        const lineStrokeWidth = Math.ceil(this.size * this.lineStrokeWidth / 100);
+        const circleStrokeWidth = Math.ceil(size * this.circleStrokeWidth / 100);
+        const lineStrokeWidth = Math.ceil(size * this.lineStrokeWidth / 100);
 
         return (
             <svg style={style} width={size} height={size}>
