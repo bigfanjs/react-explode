d9e6ea273dadbc8c65be74bf31080311abe77f82
bigfanjs <bigfanjs@gmail.com>
2018-06-18 14:08:51 +0100

    fix: syncronizes the components of explosion4 and gives the ability to update the explosion when receives new props

--- src/Explosion4.js
+++ src/Explosion4.js
@@ -2,8 +2,8 @@ import React, { Component, Fragment } from "react";
 import { TimelineMax, Power4 } from "gsap";
 
 class Explosion extends Component {
-    size = this.props.size;
-    center = this.size / 2;
+    state = { size: 400, delay: 0, repeatDelay: 0, repeat: 0 };
+    timeline = null;
     targets = [];
     counts = [7, 32];
     widths = [1.75, 0.5];
@@ -15,57 +15,105 @@ class Explosion extends Component {
         { el: null, pos: [30, 60], radius: 32.5 }
     ]
 
+    static getDerivedStateFromProps({size, delay, repeatDelay, repeat}, prevState) {
+        if (size !== prevState.size               ||
+            delay !== prevState.delay             ||
+            repeatDelay !== prevState.repeatDelay ||
+            repeat !== prevState.repeat) {
+                return { size, delay, repeatDelay, repeat };
+            }
+
+        return null;
+    }
+
     componentDidMount() {
-        const { delay = 0, repeat = 0, repeatDelay = 0, onStart, onComplete, onRepeat } = this.props;
-        const duration = 0.5;
+        this.expldoe();
+    }
+
+    componentDidUpdate() {
+        this.timeline.kill();
+        this.expldoe();
+    }
+
+    expldoe = () => {
+        const { size, delay, repeat, repeatDelay } = this.state;
+        const { onStart, onComplete, onRepeat} = this.props;
         const ease = Power4.easeOut;
-        const radius = this.size * this.radius / 100;
+        const radius = size * this.radius / 100;
+        const center = size / 2;
+        const circleStroke = Math.ceil(size * this.circleStroke / 100);
+
+        const timelines = [];
+
+        this.timeline = new TimelineMax({ repeat, repeatDelay, delay });
 
         for (let i = 0; i < 2; i++) {
             const angle = Math.PI / (this.counts[i] / 2);
 
             for (let j = 0; j < this.counts[i]; j++) {
                 const isLast = j >= this.counts[i] - 1;
-                const tl = new TimelineMax({
-                    repeat, repeatDelay,
-                    delay: delay + (i >= 1 ? 0.9 : 0),
+                const timeline = new TimelineMax({
+                    delay: (i >= 1 ? 0 : 0.15),
                     onComplete: onComplete && isLast && onComplete.bind(null, i),
                     onStart: onStart && isLast && onStart.bind(null, i),
                     onRepeat: onRepeat && isLast && onRepeat.bind(null, i)
                 });
-                const x = this.center + radius * Math.cos(j * angle);
-                const y = this.center + radius * Math.sin(j * angle);
+                const x = center + radius * Math.cos(j * angle);
+                const y = center + radius * Math.sin(j * angle);
 
                 const target = this.targets[i][j];
 
                 const start = { x2: x, y2: y };
                 const end = { x1: x, y1: y };
 
-                tl.to(target, 1, { attr: start, ease });
-                tl.to(target, 1, { attr: end, ease }, "-=0.9");
+                timeline
+                    .fromTo(
+                        target,
+                        1,
+                        { attr: { x2: center, y2: center } },
+                        { attr: start, ease }
+                    )
+                    .fromTo(
+                        target,
+                        1,
+                        { attr: { x1: center, y1: center } },
+                        { attr: end, ease }, "-=0.9"
+                    );
+
+                timelines.push(timeline);
             }
         }
 
         for (let i = 0; i < this.circles.length; i++) {
             const circle = this.circles[i];
             const el = circle.el;
-            const radius = this.size * circle.radius / 100;
-            const tl = new TimelineMax({
-                repeat, repeatDelay,
-                delay: delay + (i + 1) * 0.15,
+            const radius = size * circle.radius / 100;
+            const timeline = new TimelineMax({
+                delay: (i + 1) * 0.20,
                 onComplete: onComplete && onComplete.bind(null, 2 + i),
                 onStart: onStart && onStart.bind(null, 2 + i),
                 onRepeat: onRepeat && onRepeat.bind(null, 2 + 1)
             });
 
-            tl.to(el, 1, { attr: { r: radius / 2 }, ease });
-            tl.to(el, 1, { attr: { "stroke-width": 0 }, ease }, "-=0.9");
+            timeline
+                .fromTo(el, 1, { attr: { r: 0 } }, { attr: { r: radius / 2 }, ease })
+                .fromTo(
+                    el,
+                    1,
+                    { attr: { "stroke-width": circleStroke } },
+                    { attr: { "stroke-width": 0 }, ease },
+                    "-=0.9"
+                );
+
+            timelines.push(timeline);
         }
+
+        this.timeline.add(timelines);
     }
 
     render() {
-        const size = this.size;
-        const center = this.center;
+        const size = this.state.size;
+        const center = size / 2;
         const { style, color = "white" } = this.props;
         const circleStroke = Math.ceil(size * this.circleStroke / 100);
 
@@ -73,7 +121,7 @@ class Explosion extends Component {
             <svg style={style} width={size} height={size}>
                 <Fragment>
                     {[...Array(2)].map((_, i) => {
-                        const width = Math.ceil(this.size * this.widths[i] / 100);
+                        const width = Math.ceil(size * this.widths[i] / 100);
                         this.targets[i] = [];
 
                         return (
