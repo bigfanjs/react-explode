e4568fcbf0383dd197ede75a32938054e493ca4b
bigfanjs <bigfanjs@gmail.com>
2018-06-20 00:33:43 +0100

    feat: creates a generalized import

---
+++ src/index.js
@@ -0,0 +1,10 @@
+export { default as Explosion1 } from "./Explosion1";
+export { default as Explosion2 } from "./Explosion2";
+export { default as Explosion3 } from "./Explosion3";
+export { default as Explosion4 } from "./Explosion4";
+export { default as Explosion5 } from "./Explosion5";
+export { default as Explosion6 } from "./Explosion6";
+export { default as Explosion7 } from "./Explosion7";
+export { default as Explosion8 } from "./Explosion8";
+export { default as Explosion9 } from "./Explosion9";
+export { default as Explosion10 } from "./Explosion10";
