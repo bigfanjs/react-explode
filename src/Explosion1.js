28d6333eab898a0cb1b0acabe29808b6d311ac1a
bigfanjs <bigfanjs@gmail.com>
2019-10-29 13:34:24 +0100

    feat: upgrades explosion1

--- src/Explosion1.js
+++ src/Explosion1.js
@@ -1,107 +1,120 @@
-import React, { Component, Fragment } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { TimelineMax, Power4 } from "gsap";
 
-class Explosion extends Component {
-    state = { size: 400, delay: 0, repeatDelay: 0, repeat: 0 };
+const STROKE_WIDTH = 0.5;
+const RADIUS = 47.5;
+const COUNT = 16;
+const DURATIONS = [0.6, 1];
+const LINES = [];
 
-    size = this.props.size;
+let TIME_LINE = null;
 
-    timeline = null;
-    lines = [];
-    count = 16;
-    radius = 47.5;
-    strokeWidth = 0.5;
+export default function Explosion1({
+  size,
+  delay,
+  repeatDelay,
+  repeat,
+  style,
+  color = "white",
+  onComplete,
+  onStart,
+  onRepeat
+}) {
+  const [prevSize, setPrevSize] = useState(400);
+  const [prevDelay, setPrevDelay] = useState(0);
+  const [prevRepeatDelay, setPrevRepeatDelay] = useState(0);
+  const [prevRepeat, setPrevRepeat] = useState(0);
 
-    durations = [0.6, 1];
+  const center = size / 2;
+  const strokeWidth = Math.ceil((size * STROKE_WIDTH) / 100);
 
-    static getDerivedStateFromProps({size, delay, repeatDelay, repeat}, prevState) {
-        if (size !== prevState.size               ||
-            delay !== prevState.delay             ||
-            repeatDelay !== prevState.repeatDelay ||
-            repeat !== prevState.repeat
-        ) return { size, delay, repeatDelay, repeat };
+  const explode = useCallback(() => {
+    const ease = Power4.easeOut;
+    const angle = Math.PI / 8;
+    const radius = (prevSize * RADIUS) / 100;
+    const durations = DURATIONS;
+    const center = prevSize / 2;
+    const timelines = [];
 
-        return null;
-    }
+    TIME_LINE = new TimelineMax({
+      repeat: prevRepeat,
+      delay: prevDelay,
+      repeatDelay: prevRepeatDelay,
+      onComplete,
+      onStart,
+      onRepeat
+    });
 
-    componentDidMount() {
-        this.explode();
-    }
+    for (let i = 0; i < COUNT; i++) {
+      const x = center + radius * Math.cos(i * angle);
+      const y = center + radius * Math.sin(i * angle);
+      const target = LINES[i];
+      const start = { x2: x, y2: y };
+      const end = { x1: x, y1: y };
 
-    componentDidUpdate() {
-        this.timeline.kill();
-        this.explode();
-    }
+      const timeline = new TimelineMax();
 
-    explode() {
-        const { size, repeat, delay, repeatDelay } = this.state;
-        const ease = Power4.easeOut;
-        const angle = Math.PI / 8;
-        const radius = size * this.radius / 100;
-        const durations = this.durations;
-        const { onComplete, onStart, onRepeat } = this.props;
-        const center = size / 2;
-        const timelines = [];
-
-        this.timeline = new TimelineMax({
-            repeat, delay, repeatDelay, onComplete, onStart, onRepeat
-        });
-
-        for (let i = 0; i < this.count; i++) {
-            const x = center + radius * Math.cos(i * angle);
-            const y = center + radius * Math.sin(i * angle);
-            const target = this.lines[i];
-            const start = { x2: x, y2: y };
-            const end = { x1: x, y1: y };
-
-            const timeline = new TimelineMax();
-
-            timeline
-                .fromTo(
-                    target,
-                    durations[0],
-                    { attr: { x2: center, y2: center } },
-                    { attr: start, ease }
-                )
-                .fromTo(
-                    target,
-                    durations[1],
-                    { attr: {x1: center, y1: center} },
-                    { attr: end, ease },
-                    `-=${durations[0]}`
-                );
-
-            timelines.push(timeline);
-        }
-
-        this.timeline.add(timelines);
-    }
-
-    render() {
-        const size = this.state.size;
-        const {color="white", style} = this.props;
-        const center = size / 2;
-        const strokeWidth = Math.ceil(size * this.strokeWidth / 100);
-
-        return (
-            <svg width={size} height={size} style={style}>
-                <Fragment>
-                    {[...Array(this.count)].map((_, i) =>
-                        <line
-                            x1={center}
-                            y1={center}
-                            x2={center}
-                            y2={center}
-                            ref={(el) => this.lines[i] = el}
-                            key={i}
-                            strokeWidth={strokeWidth}
-                            stroke={color}
-                        />
-                    )}
-                </Fragment>
-            </svg>
+      timeline
+        .fromTo(
+          target,
+          durations[0],
+          { attr: { x2: center, y2: center } },
+          { attr: start, ease }
+        )
+        .fromTo(
+          target,
+          durations[1],
+          { attr: { x1: center, y1: center } },
+          { attr: end, ease },
+          `-=${durations[0]}`
         );
-    }
-}
 
-export default Explosion;
+      timelines.push(timeline);
+    }
+
+    TIME_LINE.add(timelines);
+  }, [
+    prevSize,
+    prevRepeat,
+    prevDelay,
+    prevRepeatDelay,
+    onComplete,
+    onStart,
+    onRepeat
+  ]);
+
+  useEffect(() => {
+    explode();
+  }, [explode]);
+
+  useEffect(() => {
+    TIME_LINE.kill();
+    explode();
+  });
+
+  useEffect(() => {
+    setPrevSize(size);
+    setPrevDelay(delay);
+    setPrevRepeatDelay(repeatDelay);
+    setPrevRepeat(repeat);
+  }, [size, delay, repeatDelay, repeat]);
+
+  return (
+    <svg width={size} height={size} style={style}>
+      <>
+        {[...Array(COUNT)].map((_, i) => (
+          <line
+            x1={center}
+            y1={center}
+            x2={center}
+            y2={center}
+            ref={el => (LINES[i] = el)}
+            key={i}
+            strokeWidth={strokeWidth}
+            stroke={color}
+          />
+        ))}
+      </>
+    </svg>
+  );
+}
