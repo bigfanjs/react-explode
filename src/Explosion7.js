bb629326ffa72797b0b4c26369e55f8aa37fe89c
bigfanjs <bigfanjs@gmail.com>
2019-12-01 23:52:18 +0100

    feat: upgrades explosion7

--- src/Explosion7.js
+++ src/Explosion7.js
@@ -1,115 +1,134 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { TimelineMax, Circ } from "gsap";
 
-class Explosion extends Component {
-    state = { size: 400, delay: 0, repeatDelay: 0, repeat: 0 };
-    paths = [];
-    degree = 90 * Math.PI / 180;
-    initExplosion = 4;
-    gap = 7;
-    strokeWidth = 0.5;
-    timeline = null;
+const PATHS = [];
+const DEGREE = (90 * Math.PI) / 180;
+const INIT_EXPLOSION = 4;
+const GAP = 7;
+const STROKE_WIDTH = 0.5;
 
-    static getDerivedStateFromProps({ size, delay, repeatDelay, repeat }, prevState) {
-        if (size !== prevState.size               ||
-            delay !== prevState.delay             ||
-            repeatDelay !== prevState.repeatDelay ||
-            repeat !== prevState.repeat
-        ) return { size, delay, repeatDelay, repeat };
+let TIMELINE = null;
 
-        return null;
-    }
+export default function Explosion7({
+  size,
+  delay,
+  repeatDelay,
+  repeat,
+  style,
+  color = "white",
+  onComplete,
+  onStart,
+  onRepeat
+}) {
+  const [prevSize, setPrevSize] = useState(200);
+  const [prevDelay, setPrevDelay] = useState(0);
+  const [prevRepeatDelay, setPrevRepeatDelay] = useState(0);
+  const [prevRepeat, setPrevRepeat] = useState(0);
 
-    shouldComponentUpdate({ size, delay, repeatDelay, repeat }) {
-        const state = this.state;
+  const center = prevSize / 2;
 
-        return (
-            size !== state.size               ||
-            delay !== state.delay             ||
-            repeatDelay !== state.repeatDelay ||
-            repeat !== state.repeat
+  const explode = useCallback(() => {
+    const center = prevSize / 2;
+    const ease = Circ.easeInOut;
+    const offset = INIT_EXPLOSION * 2;
+
+    const timelines = [];
+
+    for (let i = 0; i < PATHS.length; i++) {
+      const path = PATHS[i];
+      const j = Math.floor((i - offset) / INIT_EXPLOSION + 1);
+      const length = center - (i >= offset ? center * (j / 15) : 0);
+      const degree = (i < INIT_EXPLOSION ? 0 : DEGREE / 2) + DEGREE * i;
+      const xPercent = Math.cos(degree);
+      const yPercent = Math.sin(degree);
+      const xOffset = (i < INIT_EXPLOSION ? GAP : 0) * Math.cos(DEGREE * i);
+      const yOffset = (i < INIT_EXPLOSION ? GAP : 0) * Math.sin(DEGREE * i);
+      const X = center + length * xPercent;
+      const Y = center + length * yPercent;
+      const timeline = new TimelineMax({
+        delay: i < INIT_EXPLOSION ? 0 : 0.2
+      });
+
+      timeline.fromTo(
+        path,
+        0.7,
+        { attr: { x2: center, y2: center }, ease },
+        { attr: { x2: X, y2: Y }, ease }
+      );
+      timeline.fromTo(
+        path,
+        0.7,
+        { attr: { x1: center + xOffset, y1: center + yOffset }, ease },
+        { attr: { x1: X, y1: Y }, ease },
+        "-=0.5"
+      );
+
+      if (i >= offset) {
+        const transformOrigin = `${xPercent >= 0 ? 0 : 100}% ${
+          yPercent >= 0 ? 0 : 100
+        }%`;
+
+        timeline.fromTo(
+          path,
+          0.5,
+          { rotation: 0 },
+          { rotation: 90 * (j / 10), transformOrigin, ease },
+          "-=0.7"
         );
+      }
+
+      timelines.push(timeline);
     }
 
-    componentDidMount() {
-        this.explode();
-    }
+    TIMELINE = new TimelineMax({
+      delay: prevDelay,
+      repeat: prevRepeat,
+      repeatDelay: prevRepeatDelay,
+      onStart,
+      onComplete,
+      onRepeat
+    });
 
-    componentDidUpdate() {
-        this.timeline.kill();
-        this.explode();
-    }
+    TIMELINE.add(timelines);
+  }, [
+    prevSize,
+    prevDelay,
+    prevRepeat,
+    prevRepeatDelay,
+    onStart,
+    onComplete,
+    onRepeat
+  ]);
 
-    explode = () => {
-        const size = this.state.size;
-        const center = size / 2;
-        const ease = Circ.easeInOut;
-        const offset = this.initExplosion * 2;
-        const { delay, repeat, repeatDelay, onStart, onComplete, onRepeat } = this.props;
-        const timelines = [];
-        
-        for (let i = 0; i < this.paths.length; i++) {
-            const path = this.paths[i];
-            const j = Math.floor(((i - offset) / this.initExplosion) + 1);
-            const length = center - (i >= offset ? center * (j / 15) : 0);
-            const degree = (i < this.initExplosion ? 0 : this.degree / 2) + this.degree * i;
-            const xPercent = Math.cos(degree);
-            const yPercent = Math.sin(degree);
-            const xOffset = (i < this.initExplosion ? this.gap : 0) * Math.cos(this.degree * i);
-            const yOffset = (i < this.initExplosion ? this.gap : 0) * Math.sin(this.degree * i);
-            const X = center + length * xPercent;
-            const Y = center + length * yPercent;
-            const timeline = new TimelineMax({ delay: i < this.initExplosion ? 0 : 0.2 });
+  useEffect(() => {
+    if (TIMELINE) TIMELINE.kill();
+  });
 
-            timeline.fromTo(
-                path,
-                0.7,
-                { attr: { x2: center, y2: center }, ease },
-                { attr: { x2: X, y2: Y }, ease }
-            );
-            timeline.fromTo(
-                path,
-                0.7,
-                { attr: { x1: center + xOffset, y1: center + yOffset }, ease },
-                { attr: { x1: X, y1: Y }, ease },
-                "-=0.5"
-            );
+  useEffect(() => {
+    setPrevSize(size);
+    setPrevDelay(delay);
+    setPrevRepeatDelay(repeatDelay);
+    setPrevRepeat(repeat);
+  }, [size, delay, repeatDelay, repeat]);
 
-            if (i >= offset) {
-                const transformOrigin = `${xPercent >= 0 ? 0 : 100}% ${yPercent >= 0 ? 0 : 100}%`;
+  useEffect(() => {
+    explode();
+  }, [explode]);
 
-                timeline.fromTo(path, 0.5, { rotation: 0 }, { rotation: 90 * (j / 10), transformOrigin, ease }, "-=0.7");
-            }
-
-            timelines.push(timeline);
-        }
-
-        this.timeline = new TimelineMax({ delay, repeat, repeatDelay, onStart, onComplete, onRepeat });
-        this.timeline.add(timelines);
-    }
-
-    render() {
-        const size = this.state.size;
-        const { color ="white", style } = this.props;
-        const center = size / 2;
-
-        return (
-            <svg width={size} height={size} style={style}>
-                {[...Array(64)].map((_, i) => (
-                    <line
-                        key={i}
-                        x1={center + (i < this.initExplosion ? this.gap : 0) * Math.cos(this.degree * i)}
-                        y1={center + (i < this.initExplosion ? this.gap : 0) * Math.sin(this.degree * i)}
-                        x2={center}
-                        y2={center}
-                        stroke={color}
-                        strokeWidth={Math.ceil(size * this.strokeWidth / 100)}
-                        ref={(el) => this.paths[i] = el}
-                    />
-                ))}
-            </svg>
-        );
-    }
+  return (
+    <svg width={prevSize} height={prevSize} style={style}>
+      {[...Array(64)].map((_, i) => (
+        <line
+          key={i}
+          x1={center + (i < INIT_EXPLOSION ? GAP : 0) * Math.cos(DEGREE * i)}
+          y1={center + (i < INIT_EXPLOSION ? GAP : 0) * Math.sin(DEGREE * i)}
+          x2={center}
+          y2={center}
+          stroke={color}
+          strokeWidth={Math.ceil((prevSize * STROKE_WIDTH) / 100)}
+          ref={el => (PATHS[i] = el)}
+        />
+      ))}
+    </svg>
+  );
 }
-
-export default Explosion;
