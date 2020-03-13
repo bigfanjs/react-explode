37b573fcde7e8bcbe804d0028d4060180d348085
bigfanjs <bigfanjs@gmail.com>
2020-03-13 12:40:50 +0100

    feat: enhance explosion 2

--- src/Explosions/Explosion2.jsx
+++ src/Explosions/Explosion2.jsx
@@ -1,8 +1,13 @@
-import React, { useState, useEffect, useCallback, useRef } from "react";
+import React, {
+  useState,
+  useEffect,
+  useCallback,
+  useRef,
+  useMemo
+} from "react";
 import gsap, { Power4 } from "gsap";
 
-const RADIUS = 47.5;
-const STROKE_WIDTH = 1;
+const STROKE_WIDTH = 2;
 
 let TIME_LINE = null;
 
@@ -17,19 +22,21 @@ export default function Explosion2({
   onStart,
   onRepeat
 }) {
+  const circleRef = useRef();
+
   const [prevSize, setPrevSize] = useState(400);
   const [prevDelay, setPrevDelay] = useState(0);
   const [prevRepeatDelay, setPrevRepeatDelay] = useState(0);
   const [prevRepeat, setPrevRepeat] = useState(0);
-  const circleRef = useRef();
 
-  const center = size / 2;
-  const strokeWidth = Math.ceil((size * STROKE_WIDTH) / 100);
+  const center = useMemo(() => prevSize / 2, [prevSize]);
+  const strokeWidth = useMemo(
+    () => Math.ceil((prevSize * STROKE_WIDTH) / 100),
+    [prevSize]
+  );
 
   const explode = useCallback(() => {
     const ease = Power4.easeOut;
-    const radius = (prevSize * RADIUS) / 100;
-    const strokeWidth = Math.ceil((prevSize * STROKE_WIDTH) / 100);
 
     TIME_LINE = gsap.timeline({
       repeat: prevRepeat,
@@ -43,35 +50,30 @@ export default function Explosion2({
     TIME_LINE.fromTo(
       circleRef.current,
       1,
-      { attr: { r: 0 } },
-      { attr: { r: radius }, ease }
-    )
-      .fromTo(
-        circleRef.current,
-        0.8,
-        { attr: { "stroke-width": strokeWidth, opacity: 1 }, ease },
-        { attr: { "stroke-width": 0, opacity: 0 }, ease },
-        "-=0.5"
-      )
-      .set(circleRef.current, { attr: { "stroke-width": strokeWidth } }, "-=1");
+      { scale: 0, transformOrigin: "center" },
+      { scale: 1, ease }
+    );
+    TIME_LINE.fromTo(
+      circleRef.current,
+      0.8,
+      { attr: { "stroke-width": strokeWidth, opacity: 1 }, ease },
+      { attr: { "stroke-width": 0, opacity: 0 }, ease },
+      "-=0.5"
+    ).set(circleRef.current, { attr: { "stroke-width": strokeWidth } }, "-=1");
   }, [
-    prevSize,
     prevRepeat,
     prevDelay,
     prevRepeatDelay,
     onComplete,
     onStart,
-    onRepeat
+    onRepeat,
+    strokeWidth
   ]);
 
   useEffect(() => {
+    if (TIME_LINE) TIME_LINE.kill();
     explode();
   }, [explode]);
-
-  useEffect(() => {
-    TIME_LINE.kill();
-    explode();
-  });
 
   useEffect(() => {
     setPrevSize(size);
@@ -81,11 +83,11 @@ export default function Explosion2({
   }, [size, delay, repeatDelay, repeat]);
 
   return (
-    <svg width={size} height={size} style={style}>
+    <svg width={prevSize} height={prevSize} style={style}>
       <circle
         cx={center}
         cy={center}
-        r={0}
+        r="47%"
         strokeWidth={strokeWidth}
         stroke={color}
         fill="none"
